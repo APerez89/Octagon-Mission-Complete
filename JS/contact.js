0821299fe8b7833208ead81d0c655f216678ec6a
Alex <alexperez689@gmail.com>
2018-02-16 15:23:04 -0500

    JS Error Fixed
    Changed div to form, removed JS code to fix error. a tag changed to
    button

--- JS/contact.js
+++ JS/contact.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementsByTagName('.contact-form')[0];
     const fname = document.querySelector('#fname');
     const lname = document.querySelector('#lname');
     const email = document.querySelector('#email');
@@ -14,11 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
         states.appendChild(option);
     })
 
-    // Submit form
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        sendInfo();
-    });
 });
 
 const statesArray = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
